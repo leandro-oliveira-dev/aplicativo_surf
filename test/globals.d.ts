8ea4b945dc0e31e09cac1c63641751380185fe6d
Leandro Oliveira da Silva <lleandro.silva@outlook.com>
2024-11-16 19:34:03 -0300

    configurações do projeto

---
+++ test/globals.d.ts
@@ -0,0 +1,14 @@
+import { SuperTest, Test } from "supertest";
+
+/**
+ * @Update 2022 - This has logic has been update to support newer Node.js versions
+ * that don't have NodeJS as a global type
+ */
+declare global {
+  //eslint-disable-next-line no-var
+  var testRequest: import("supertest").SuperTest<import("supertest").Test>;
+}
+declare global {
+  var testRequest: SuperTest<Test>;
+}
+export {};
